059e94acf13041dec761a09b489a9828ee90e890
kok73r <kok73r@gmail.com>
2022-05-02 15:29:32 +0300

    remake 2

--- src/dream/team/homework/lesson2/task1.java
+++ src/dream/team/homework/lesson2/task1.java
@@ -3,15 +3,18 @@ package dream.team.homework.lesson2;
 public class task1 {
     //task1
     public static void main(String[] args) {
-        int a = 20;
+        int a = 3;
         System.out.println("a = " + a);
-        int b = 5;
+        int b = 8;
         System.out.println("b = " + b);
 
-        System.out.println();
-        System.out.println("a= " + b);
-        System.out.println("b= " + a);
+        int c = a;
+        a = b;
+        b = c;
 
+        System.out.println();
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
 
 
     }

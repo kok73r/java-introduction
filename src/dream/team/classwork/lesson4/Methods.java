7bd268829a778f18fe43b0e0595f86b3e1cac47c
kok73r <kok73r@gmail.com>
2022-05-06 22:39:23 +0300

    methods, HomeWork Task1

--- src/dream/team/classwork/lesson4/Methods.java
+++ src/dream/team/classwork/lesson4/Methods.java
@@ -6,7 +6,7 @@ public class Methods {
     public static void main(String[] args) {
 
         //тут методы не создаём
-        printMessage();
+        printMessage("Teh");
 
         int number = generateRandom();
         System.out.println(number);
@@ -14,23 +14,24 @@ public class Methods {
         int randomInt = generateRandom(18, 50);
         System.out.println(randomInt);
 
-        printMessage("test");
+
     }
 
     public static void printMessage() {
         System.out.println("message");
     }
+
     public static void printMessage(String message) {
         System.out.println(message);
     }
 
-    public static int generateRandom(){
-        return (int)(Math.random() * 100);
+    public static int generateRandom() {
+        return (int) (Math.random() * 100);
     }
 
-    public static int generateRandom( int min, int max){
+    public static int generateRandom(int min, int max) {
         printMessage();
-        return (int) (Math.random() * (max - min) +min);
+        return (int) (Math.random() * (max - min) + min);
     }
 }
 
--- src/dream/team/homework/lesson4/Task1.java
+++ src/dream/team/homework/lesson4/Task1.java
@@ -1,20 +1,29 @@
-/*
 package dream.team.homework.lesson4;
 
 public class Task1 {
     public static void main(String[] args) {
 
-        public static void printMessage(String message) {
-            System.out.println(printAirplaneInfo);
-    }
+        printAirplaneInfo("Jet", "F-22 Raptor");
+        printAirplaneInfo("Airliner", "Aerobus 737", "120");
+        printAirplaneInfo("Airliner", "Boeing 777", "180" , "28");
 
-    public static void printAirplaneInfo() {
-
-    }
-    public static void printAirplaneInfo() {
 
     }
-    public static void printAirplaneInfo() {
+
+    public static void printAirplaneInfo(String name, String type) {
+        System.out.println("Type of aircraft: " + name + ", Model: " + type);
 
     }
-}*/
+
+    public static void printAirplaneInfo(String name, String type, String numberOfEconomClassSeats) {
+        System.out.println("Type of aircraft: " + name + ", Model: " + type + ", number of economy class seats " + numberOfEconomClassSeats);
+
+    }
+
+    public static void printAirplaneInfo(String name, String type, String numberOfEconomClassSeats, String numberOfbusinessClassSeats) {
+        System.out.println("Type of aircraft: " + name + ", Model: " + type + ", number of economy class seats " + numberOfEconomClassSeats + ", number of business class seats " + numberOfbusinessClassSeats);
+
+    }
+
+
+}

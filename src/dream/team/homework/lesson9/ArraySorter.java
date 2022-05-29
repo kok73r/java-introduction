639a3b6e3a1beaf68d52f28e42ad72e5bd133a80
kok73r <kok73r@gmail.com>
2022-05-29 19:48:13 +0300

    add task homework.lesson8 ArraySorter

---
+++ src/dream/team/homework/lesson9/ArraySorter.java
@@ -0,0 +1,13 @@
+package dream.team.homework.lesson9;
+
+import java.util.Arrays;
+
+public class ArraySorter {
+    public static void main(String[] args) {
+
+        int[] array = {3, 6, 0, 4, 0, 3, 0, 8, 5};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}

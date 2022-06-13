3990c4a39eefc9bb5abfc32de343f073fd4c3a44
kok73r <kok73r@gmail.com>
2022-06-13 20:05:57 +0300

    add task homework11 CatComparison

---
+++ src/dream/team/homework/homework11/Cat.java
@@ -0,0 +1,46 @@
+package dream.team.homework.homework11;
+
+public class Cat {
+    String name;
+    int age;
+    int weight;
+    String breed;
+
+    public Cat(String name, int age, int weight, String breed) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.breed = breed;
+
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Cat) {
+            Cat cat = (Cat) obj;
+            if (age == cat.age && weight == cat.weight) {
+                System.out.println("These cats are the same");
+                return true;
+
+            }
+        }
+        System.out.println("These cats are different");
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+
+}
